b0145d0dccd71a8495d9ff1eceb3625dfae4b03c
gapur <kaloycant@gmail.com>
2022-10-07 16:52:04 +0200

    use Asserts from nodejs

--- 1/tests/capitalize.test.js
+++ 1/tests/capitalize.test.js
@@ -1,11 +1,10 @@
+import { strict as assert } from 'assert';
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
+
+assert.notEqual(capitalize('hello'), 'hello');
 
 console.log('Все тесты пройдены!');
